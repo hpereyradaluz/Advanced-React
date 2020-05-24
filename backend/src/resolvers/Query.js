3261a2639f06680297ffce7e8dae97e8e5cb93f4
Hernan <hpereyradaluz@gmail.com>
2020-05-23 22:31:30 -0300

    19 - update items with queries and mutations

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -2,6 +2,7 @@ const { forwardTo } = require("prisma-binding");
 
 const Query = {
   items: forwardTo("db"),
+  item: forwardTo("db"),
   // when the query is the de samen in my yoga and as it is in my prisma
   // i dont need to write this code
   // it serve if i dont need authentication and simpe mock up somethin righ now
