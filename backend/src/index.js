d76cc9ecc19f88aee07d0b9724db6297e6f3eebe
Hernan <hpereyradaluz@gmail.com>
2020-09-01 20:35:20 -0300

    user signup and permmissions flow

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,21 +1,21 @@
-require('dotenv').config({ path: 'variables.env' });
-const createServer = require('./createServer');
-const db = require('./db');
-
+const cookieParser = require("cookie-parser");
+require("dotenv").config({ path: "variables.env" });
+const createServer = require("./createServer");
+const db = require("./db");
 
 const server = createServer();
 
-// TODO use express middleware to handle cookies (jwt)
+server.express.use(cookieParser());
 // TODO use express middleware to populate current user
 
 server.start(
   {
-  cors: {
-    credentials: true,
-    origin: process.env.FRONTEND_URL,
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
     },
-  }, 
-  deets => {
+  },
+  (deets) => {
     console.log(`Server is now running on port 
     http:/localhost:${deets.port}`);
   }
